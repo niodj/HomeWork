7f309a270fd544fd89bcea72e7fd204f69447cba
AntonPotapenko <a0501856767@gmail.com>
2023-08-18 17:34:03 +0200

    HW2

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -3,13 +3,14 @@ import s from './App.module.css'
 
 import HW5 from "../s2-homeworks/hw05/HW5";
 import HW1 from "../s2-homeworks/hw01/HW1";
+import HW2 from "../s2-homeworks/hw02/HW2";
 
 function App() {
     return (
         <div className={s.App}>
             <HW1/>
             {/*раскомментировать по ходу выполнения*/}
-            {/*<HW2/>*/}
+            <HW2/>
             {/*<HW3/>*/}
             {/*<HW4/>*/}
 
