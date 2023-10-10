20590b95fe44b3ae0fc5259fb9ac8c31b8b6eb5b
AntonPotapenko <a0501856767@gmail.com>
2023-10-10 20:45:37 +0200

    update code

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,30 +1,42 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
-import s from './SuperButton.module.css'
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import s from "./SuperButton.module.css";
 
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
-type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement>
+type DefaultButtonPropsType = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
-    xType?: string
-}
+  xType?: string;
+};
 
-const SuperButton: React.FC<SuperButtonPropsType> = (
-    {
-        xType,
-        className,
-        disabled,
-        ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
-    }
-) => {
-    const finalClassName =  disabled ? s.disabled : xType === 'red' ? s.red : xType==='secondary' ? s.secondary :  s.default + (className ? ' ' + className : '')
-    return (
-        <button
-            disabled={disabled}
-            className={finalClassName}
-            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
-        />
-    )
-}
+const SuperButton: React.FC<SuperButtonPropsType> = ({
+  xType,
+  className,
+  disabled,
+  ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
+}) => {
+  const finalClassName =
+    s.button +
+    " " +
+    (disabled
+      ? s.disabled
+      : xType === "red"
+      ? s.red
+      : xType === "secondary"
+      ? s.secondary
+      : s.default) +
+    " " +
+    (className ? className : "");
 
-export default SuperButton
+  return (
+    <button
+      disabled={disabled}
+      className={finalClassName}
+      {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+    />
+  );
+};
+
+export default SuperButton;
