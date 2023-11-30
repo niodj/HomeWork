d52a563f39e9a2939258509bdfe3f5df1ad13067
Anton Potapenko <a0501856767@gmail.com>
2023-11-30 22:04:09 +0100

    change CV path page

--- src/s2-homeworks/hw06/localStorage/localStorage.ts
+++ src/s2-homeworks/hw06/localStorage/localStorage.ts
@@ -7,8 +7,10 @@ export function saveState<T>(key: string, state: T) {
 
 // и вот вам функция для получения сохранённого объекта в памяти браузера:
 export function restoreState<T>(key: string, defaultState: T) {
+
     let state = defaultState
     const stateAsString = localStorage.getItem(key)
+   
     if (stateAsString !== null) state = JSON.parse(stateAsString) as T
     return state
 }
